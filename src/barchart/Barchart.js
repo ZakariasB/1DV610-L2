7f4e723ed6412ca9825b58d11d51954f87c99fc5
Zakarias Bergcrona <zb222cw@student.lnu.se>
2023-09-24 20:41:25 +0200

    Adds BarChart class and constructor

--- src/barchart/Barchart.js
+++ src/barchart/Barchart.js
@@ -1,3 +1,12 @@
 export default class BarChart {
-
+  constructor(options) {
+    this.canvas = document.getElementById(options.canvasId)
+    this.context = this.canvas.getContext('2d')
+    this.data = options.data ||  []
+        this.labels = options.labels || []
+        this.colors = options.colors || []
+        this.width = this.canvas.width
+        this.height = this.canvas.height
+        this.padding = options.padding || 10
+  }
 }
