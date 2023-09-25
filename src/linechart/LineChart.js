8a2e67569be61e0aa08476b42f4bca06af48df35
Zakarias Bergcrona <zb222cw@student.lnu.se>
2023-09-25 21:45:41 +0200

    Adds draw and clear method to LineChart class

--- src/linechart/LineChart.js
+++ src/linechart/LineChart.js
@@ -8,4 +8,37 @@ export default class LineChart {
         this.padding = 50
     }
 
+    draw() {
+        if (this.data.length === 0) {
+            console.log("Error: Empty data")
+            return
+        }
+
+        let maxValue = Math.max(...this.data)
+        let minValue = Math.min(...this.data)
+
+        let horizontalStep = (this.canvas.width - 2 * this.padding) / (this.data.length - 1)
+        let verticalScale = (this.canvas.height - 2 * this.padding) / (maxValue - minValue)
+
+        this.context.beginPath()
+
+        this.data.forEach((value, index) => {
+            let x = this.padding + index * horizontalStep
+            let y = this.canvas.height - this.padding - (value - minValue) * verticalScale
+
+            if (index === 0) {
+                this.context.moveTo(x, y)
+            } else {
+                this.context.lineTo(x, y)
+            }
+        })
+
+        this.context.strokeStyle = this.color
+        this.context.stroke()
+    }
+
+    clear () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+
 }
