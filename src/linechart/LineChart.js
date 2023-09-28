bbeaba91f2f3efcc648d2511b48f75134ba16f14
Zakarias Bergcrona <zb222cw@student.lnu.se>
2023-09-28 15:15:49 +0200

    Fixes small bug in LineChart

--- src/linechart/LineChart.js
+++ src/linechart/LineChart.js
@@ -14,7 +14,7 @@ export default class LineChart {
     this.context = this.canvas.getContext('2d')
     this.data = options.data || []
     this.labels = options.labels || []
-    this.color = options.colors || '#000'
+    this.color = options.color || '#000'
     this.padding = 50
   }
 
