572599c7f4f0fc0d2349736e46da5e80d51a259e
Zakarias Bergcrona <zb222cw@student.lnu.se>
2023-10-22 16:04:33 +0200

    Created new base class BaseChart

---
+++ src/basechart/BaseChart.js
@@ -0,0 +1,38 @@
+
+export default class BaseChart {
+    constructor(options) {
+        if (!this.isValidCanvasId(options.canvasId)) {
+            throw new TypeError('Need a valid reference to a canvas element') 
+        }
+
+        if (!this.isValidData(options.data)) {
+            throw new TypeError('Data needs to be an array of data and not empty') 
+        }
+
+        this.canvas = document.getElementById(options.canvasId)
+        this.context = this.canvas.getContext('2d')
+        this.data = options.data
+
+
+
+
+    }
+
+    isValidCanvasId(canvasId) {
+        return typeof canvasId === 'string' && document.getElementById(canvasId)
+    }
+
+    isValidData(data) {
+        return typeof data === 'array' && data[0]
+    }
+
+  /**
+   * Clears the html canvas element.
+   */
+  clear() {
+    this.context.clearRect(0, 0, this.width, this.height)
+  }
+
+
+
+}
